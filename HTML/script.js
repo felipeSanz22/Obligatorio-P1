5cd9596dbe5be500d68bbb00639a730a2a7704a3
Felipe Sanz <sanzroballo.felipe@gmail.com>
2022-10-21 19:26:07 -0300

    Actualizacion

--- HTML/script.js
+++ HTML/script.js
@@ -24,22 +24,25 @@ function ocultarTodo(){
     document.querySelector("#ListaDeCargaPeligrosa").style.display="none"
 }
 
+
+
+
 document.querySelector("#btnIngresarComoEmpresa").addEventListener("click", IngresarComoEmpresa);
 document.querySelector("#btnIngresarComoImportador").addEventListener("click", IngresarComoImportador);
-document.querySelector("#btnRegistrarImportadores").addEventListener("click", RegistrarImportadores);
+document.querySelector("#btnVolverAlingreso").addEventListener("click", VolverAlIngreso);
 document.querySelector("#btnIngresoGeneral").addEventListener("click", IngresoGeneral);
-document.querySelector("#Registro").addEventListener("click", IngresoGeneral);
+document.querySelector("#Registro").addEventListener("click", Registro);
+document.querySelector("#btnRegistrarImportadores").addEventListener("click", RegistroDeImportadores);
 document.querySelector("#btnLogoutImportador").addEventListener("click", LogoutImportador);
-document.querySelector("#btnRegistrarImportadores").addEventListener("click", RegistrarImportadores);
 document.querySelector("#btnMenuImportadorToSolicitudCarga").addEventListener("click", MenuImportadorToSolicitudCarga);
 document.querySelector("#btnMenuImportadorToSolicitudesPendientes").addEventListener("click", ImportadorToSolicitudesPendientes);
 document.querySelector("#btnMenuImportadorToInformacionEstadistica").addEventListener("click", MenuImportadorToInformacionEstadistica);
 document.querySelector("#btnSolicitudCargaToMenuImportador").addEventListener("click", SolicitudCargaToMenuImportador);
-document.querySelector("#btnSolicitudesPendientesToMenuImportador").addEventListener("click", SolicitudesPendientesToMenuImportador);
-document.querySelector("#btnInformacionEstadisticaToMenuImportador").addEventListener("click", InformacionEstadisticaToMenuImportador);
-document.querySelector("#btnAccionarCargaImportador").addEventListener("click", AccionarCargaImportador);
-document.querySelector("#btnConfirmarCancelacion").addEventListener("click", ConfirmarCancelacion);
-document.querySelector("#btnCancelarCancelacion").addEventListener("click", CancelarCancelacion);
+document.querySelector("#btnAccionarCargaImportador").addEventListener("click", InformacionEstadisticaToMenuImportador);
+document.querySelector("#btnSolicitudesPendientesToMenuImportador").addEventListener("click", AccionarCargaImportador);
+document.querySelector("#btnConfirmarCancelacion").addEventListener("click", SolicitudesPendientesToMenuImportador);
+document.querySelector("#btnCancelarCancelacion").addEventListener("click", ConfirmarCancelacion);
+document.querySelector("#btnInformacionEstadisticaToMenuImportador").addEventListener("click", CancelarCancelacion);
 document.querySelector("#btnLogoutEmpresa").addEventListener("click", LogoutEmpresa);
 document.querySelector("#btnMenuEmpresaToCrearViaje").addEventListener("click", MenuEmpresaToCrearViaje);
 document.querySelector("#btnMenuEmpresaToAsignar").addEventListener("click", MenuEmpresaToAsignar);
@@ -51,39 +54,127 @@ document.querySelector("#btnAsignarToMenuEmpresa").addEventListener("click", Asi
 document.querySelector("#btnAsignarViajeBuque").addEventListener("click", AsignarViajeBuque);
 document.querySelector("#btnRolloverToMenuEmpresa").addEventListener("click", RolloverToMenuEmpresa);
 document.querySelector("#btnRollover").addEventListener("click", Rollover);
-document.querySelector("#btnManifiesToMenuEmpresa").addEventListener("click", ManifiesToMenuEmpresa);
-document.querySelector("#btnVerBuquesToMenuEmpresa").addEventListener("click", VerBuquesToMenuEmpresa);
+document.querySelector("#btnVerBuquesToMenuEmpresa").addEventListener("click", ManifiesToMenuEmpresa);
+document.querySelector("#btnManifiesToMenuEmpresa").addEventListener("click", VerBuquesToMenuEmpresa);
 document.querySelector("#btnVerificarCargasPeligrosas").addEventListener("click", VerificarCargasPeligrosas);
 document.querySelector("#btnPeligrosaToManifiesto").addEventListener("click", PeligrosaToManifiesto);
 
-let contadorImportadores=1
-let listaEmpresas= new Array()
-let listaImportadores= new Array()
+let contadorImportadores=
 
-class Empresa{constructor(unNombre,unUsuario,unaContraseña){
+
+class Empresa{constructor(unId,unNombre,unUsuario,unaContraseña){
+    this.idEmpresa=unId;
     this.nombreEmpresa=unNombre;
     this.usuarioEmpresa=unUsuario;
     this.contraseñaEmpresa=unaContraseña;
     }
 }
 
-class Importador{constructor(unNombre,unUsuario,unaContraseña,unaFoto){
+class Importador{constructor(unNombre,unUsuario,unaContraseña,unaFoto,unEstado){
     this.nombreImportador=unNombre;
     this.usuarioImportador=unUsuario;
     this.contraseñaimportador=unaContraseña;
     this.fotoImportador=unaFoto;
+    this.estado=unEstado
     contadorImportador++
 
     }
 }
 
-let empresa1= new Empresa("FahrmutCompany","Fahrmut","Ort22");
-let empresa2= new Empresa("BooBooCompany","BooBoo","Ort22");
-let empresa3= new Empresa("LicanCompany","Licantropiano","Ort22");
-let empresa4= new Empresa("AlfitorexCompany","Alfitorex","Ort22");
-listaEmpresas.push(empresa1)
-listaEmpresas.push(empresa2)
-listaEmpresas.push(empresa3)
-listaEmpresas.push(empresa4)
+let listaEmpresas= new Array()
+let listaImportadores= new Array()
 
+let empresa1= new Empresa(1,"FahrmutCompany","Fahrmut","Ort22");
+let empresa2= new Empresa(2,"BooBooCompany","BooBoo","Ort22");
+let empresa3= new Empresa(3,"LicanCompany","Licantropiano","Ort22");
+let empresa4= new Empresa(4,"AlfitorexCompany","Alfitorex","Ort22");
+listaEmpresas.push(empresa1);
+listaEmpresas.push(empresa2);
+listaEmpresas.push(empresa3);
+listaEmpresas.push(empresa4);
+
+let  importador1= new Importador("Matias","Mati","Ort22","habilitado");
+let  importador2= new Importador("Hatsujikan","Criterio","Ort22","habilitado");
+let  importador3= new Importador("Kuky","Kira","Ort22","habilitado");
+let  importador4= new Importador("DSonic","Ludicolo","Ort22","habilitado");
+listaImportadores.push(importador1);
+listaImportadores.push(importador2);
+listaImportadores.push(importador3);
+listaImportadores.push(importador4);
+
+
+function IngresarComoEmpresa(){
+    document.querySelector("#PantallaDeInicio").style.display="none"
+    document.querySelector("#IngresarEnLaAplicacion").style.display="block"
+}
+
+function IngresarComoImportador(){
+    document.querySelector("#PantallaDeInicio").style.display="none"
+    document.querySelector("#IngresarEnLaAplicacion").style.display="block"
+}
+
+function VolverAlIngreso(){
+    inicio()
+}
+
+function IngresoGeneral(){
+    let name=document.querySelector("#txtNombreDeUsuario").value
+    let password=document.querySelector("#txtContraseñaIngreso").value
+    
+}
+
+
+function validarClave(unaClave){
+    let valida= true;
+    if (unaClave.length <5){valida= false}
+    if (contarMayusculas(unaClave)<1){valida= false}
+    if (contarMinusculas(unaClave)<1){valida= false}
+    if (contarNumeros(unaClave)<1){valida= false}
+    return valida
+}
+
+function VerificrUsuario(unUsuario){
+    let valida=false;
+    for (let i in listaEmpresas){
+       
+            }
+    return valida
+}
+
+function contarMayusculas(texto){
+   
+    texto=texto.replaceAll(" ", "");
+    let cantidadMayusculas=0
+    for (let i=0;i<texto.length;i++){
+        if ((texto.charAt(i)==texto.charAt(i).toUpperCase())
+         && (isNaN(texto.charAt(i)))) {
+            cantidadMayusculas++
+        }
+    }
+    return cantidadMayusculas 
+}
+
+function contarMinusculas(texto){
+   
+    texto=texto.replaceAll(" ", "");
+    let cantidadMinusculas=0
+    for (let i=0;i<texto.length;i++){
+        if ((texto.charAt(i)==texto.charAt(i).toLowerCase()) 
+        && (isNaN(texto.charAt(i)))){
+            cantidadMinusculas++ 
+        }
+    }
+    return cantidadMinusculas
+}
+function contarNumeros(texto){
+    
+    texto=texto.replaceAll(" ", "");
+    let cantidadNumeros=0
+    for (let i=0;i<texto.length;i++){
+        if (!isNaN(texto.charAt(i))){
+            cantidadNumeros++
+        }
+    }
+    return cantidadNumeros 
+}
 
